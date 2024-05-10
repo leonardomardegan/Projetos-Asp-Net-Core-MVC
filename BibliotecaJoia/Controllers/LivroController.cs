bae9ee68dc50b46ad86646972c1d36f962661b86
Leonardo Mardegan <leonardopdrums@hotmail.com>
2024-05-10 09:59:40 -0300

    Detalhes

--- BibliotecaJoia/Controllers/LivroController.cs
+++ BibliotecaJoia/Controllers/LivroController.cs
@@ -84,5 +84,29 @@ namespace BibliotecaJoia.Controllers
                 throw ex;
             }
         }
+
+        public IActionResult Details(string? id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return NotFound();
+
+            var livro = _livroService.PesquisarPorId(id);
+            if (livro == null)
+                return NotFound();
+
+            return View(livro);
+        }
+
+        public IActionResult Delete(string? id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return NotFound();
+
+            var livro = _livroService.PesquisarPorId(id);
+            if (livro == null)
+                return NotFound();
+
+            return View(livro);
+        }
     }
 }
