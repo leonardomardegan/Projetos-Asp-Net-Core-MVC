4d976fef013672ac4b817f0e8ced1c6eb20fdc3d
Leonardo Mardegan <leonardopdrums@hotmail.com>
2024-06-05 10:47:58 -0300

    Consultas SQL - Gerenciador de Query

---
+++ BibliotecaJoia/Models/Enums/TSql.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaJoia.Models.Enums
+{
+    public enum TSql
+    {
+        CADASTRAR_LIVRO,
+        LISTAR_LIVRO,
+        PESQUISAR_LIVRO,
+        ATUALIZAR_LIVRO,
+        EXCLUIR_LIVRO
+    }
+}
---
+++ BibliotecaJoia/Models/Repositories/SqlManager.cs
@@ -0,0 +1,41 @@
+﻿using BibliotecaJoia.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaJoia.Models.Repositories
+{
+    public class SqlManager
+    {
+        public static string GetSql(TSql tsql)
+        {
+            var sql = "";
+
+            switch (tsql)
+            {
+                case TSql.CADASTRAR_LIVRO:
+                    sql = "insert into livro (id, nome, autor, editora) values (@id, @nome, @autor, @editora)";
+                    break;
+
+                case TSql.LISTAR_LIVRO:
+                    sql = "select id, nome, autor, editora from livro order by nome";
+                    break;
+
+                case TSql.PESQUISAR_LIVRO:
+                    sql = "select id, nome, autor, editora from livro where id = @id";
+                    break;
+
+                case TSql.ATUALIZAR_LIVRO:
+                    sql = "update livro set nome = @nome, autor = @autor, editora = @editora from livro where id = @id";
+                    break;
+
+                case TSql.EXCLUIR_LIVRO:
+                    sql = "delete from livro where id = @id";
+                    break;
+            }
+
+            return sql;
+        }
+    }
+}
