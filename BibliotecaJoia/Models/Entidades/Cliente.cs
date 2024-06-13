2352a798f63c5e2204f9ca33689efbc5eeb8693b
Leonardo Mardegan <leonardopdrums@hotmail.com>
2024-06-13 14:41:23 -0300

    Mapeando as Entidades

---
+++ BibliotecaJoia/Models/Entidades/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaJoia.Models.Entidades
+{
+    public class Cliente : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Fone { get; set; }
+        public StatusCliente StatusClienteId { get; set; }
+    }
+}
---
+++ BibliotecaJoia/Models/Entidades/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaJoia.Models.Entidades
+{
+    public class Livro : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+        public StatusLivro StatusLivroId { get; set; }
+    }
+}
